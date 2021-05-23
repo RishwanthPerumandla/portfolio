1f4f44b4c948b078631e3b052657ead117f2a45c
RishwanthPerumandla <rishwanth100@gmail.com>
2021-05-23 14:33:43 +0530

    Added Analytics

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-import {BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import SinglePost from './components/SinglePost'
 import Post from './components/Post'
 import Project from './components/Project'
 import Resume from './components/Resume'
+import ReactGA from 'react-ga'
+import Error from './components/Error'
 
-import Error from "./components/Error";
+function initializeAnalytics() {
+  ReactGA.initialize('UA-191213778-1')
+  ReactGA.pageview('/HomePage')
+}
 
 function App() {
   return (
     <BrowserRouter>
-    <Switch>
-      <Route component={Home} path='/' exact />
-      <Route component={About} path='/about' />
-      <Route component={SinglePost} path='/post/:slug' />
-      <Route component={Post} path='/post' />
-      <Route component={Project} path='/project' />
-      <Route component={Resume} path='/resume' />
+      <Switch>
+        <Route component={Home} path='/' exact />
+        <Route component={About} path='/about' />
+        <Route component={SinglePost} path='/post/:slug' />
+        <Route component={Post} path='/post' />
+        <Route component={Project} path='/project' />
+        <Route component={Resume} path='/resume' />
 
-      <Route component={Error} />
-
-    </Switch>
+        <Route component={Error} />
+      </Switch>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/About.js
+++ src/components/About.js
@@ -1,29 +1,48 @@
 import React from 'react'
 
-export default function About(){
-    return (
-        <div data-aos="fade-up" id="about" className="about bg-black-100 container px-8 md:mb-12 bg-black overflow-hidden max-w-6xl mx-auto min-h-full mt-8 sm:mt-8 sm:px-1 md:px-4 md:mt-18 lg:mt-18 xl:mt-20">
-            <div className="mt-6 mx-auto lg:px-4 md:px-2 sm:mt-8 md:mt-2 lg:mt-16 xl:mt-18">
-                <h1 className="subheadings mt-3 text-4xl text-indigo-500 sm:mt-5 sm:text-5xl md:text-6xl sm:mx-auto md:mt-5">WHO AM I?</h1>
-                <div className="about-content mt-10 sm:mt-10 md:mt-12">
-                <p className="mt-3 text-gray-500 sm:mt-5 md:mt-5 sm:mb-10  md:mb-10  sm:text-2xl md:text-4xl ">
-                I’m Rishwanth Perumandla, Entrepreneur and Computer Science Student based in Hyderabad, India. 
-
-                 </p>
-                 <p className="mt-3 text-gray-500 sm:mt-5 md:mt-5 sm:mb-10  md:mb-10  sm:text-2xl md:text-4xl ">
-
-                 As a Co-Founder and Product Designer in <a href="https://www.rivach.com" className="hover:text-indigo-500 underline">Rivac Hybrids</a>, I’ve designed and developed multiple web and mobile experiences for small businesses and organizations.  
-                </p>
-                  <p className="mt-3 text-gray-500 sm:mt-5 md:mt-5 sm:mb-10  md:mb-10  sm:text-2xl md:text-4xl ">
-
-                  I believe in a great statement<span className="hover:text-indigo-500 text-indigo-500"> "Specialization is for insects”</span>, Other than Web and Mobile design and development, I’ve been into 3D Game Design and Development with UNREAL ENGINE and also into Filmmaking.
-                  </p>
-                     <p className="mt-3 text-gray-500 sm:mt-5  md:mt-5 sm:mb-10  md:mb-10  sm:text-2xl md:text-4xl ">
-                     I always have a curiosity to get the answers for questions and I like Watching movies which provokes my thoughts.
-                        </p>
-                </div>
-               
-            </div>
+export default function About() {
+  return (
+    <div
+      data-aos='fade-up'
+      id='about'
+      className='about bg-black-100 container px-8 md:mb-12 bg-black overflow-hidden max-w-6xl mx-auto min-h-full mt-8 sm:mt-8 sm:px-1 md:px-4 md:mt-18 lg:mt-18 xl:mt-20'
+    >
+      <div className='mt-6 mx-auto lg:px-4 md:px-2 sm:mt-8 md:mt-2 lg:mt-16 xl:mt-18'>
+        <h1 className='subheadings mt-3 text-4xl text-indigo-500 sm:mt-5 sm:text-5xl md:text-6xl sm:mx-auto md:mt-5'>
+          WHO AM I?
+        </h1>
+        <div className='about-content mt-10 sm:mt-10 md:mt-12'>
+          <p className='mt-3 text-gray-500 sm:mt-5 md:mt-5 sm:mb-10  md:mb-10  sm:text-2xl md:text-4xl '>
+            I’m Rishwanth Perumandla, Entrepreneur and Computer Science Student
+            based in Hyderabad, India.
+          </p>
+          <p className='mt-3 text-gray-500 sm:mt-5 md:mt-5 sm:mb-10  md:mb-10  sm:text-2xl md:text-4xl '>
+            As a Co-Founder and Product Designer in{' '}
+            <a
+              href='https://www.rivach.com'
+              className='hover:text-indigo-500 underline'
+            >
+              Rivach
+            </a>
+            , I’ve designed and developed multiple web and mobile experiences
+            for small businesses and organizations.
+          </p>
+          <p className='mt-3 text-gray-500 sm:mt-5 md:mt-5 sm:mb-10  md:mb-10  sm:text-2xl md:text-4xl '>
+            I believe in a great statement
+            <span className='hover:text-indigo-500 text-indigo-500'>
+              {' '}
+              "Specialization is for insects”
+            </span>
+            , Other than Web and Mobile design and development, I’ve been into
+            3D Game Design and Development with UNREAL ENGINE and also into
+            Filmmaking.
+          </p>
+          <p className='mt-3 text-gray-500 sm:mt-5  md:mt-5 sm:mb-10  md:mb-10  sm:text-2xl md:text-4xl '>
+            I always have a curiosity to get the answers for questions and I
+            like Watching movies which provokes my thoughts.
+          </p>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
